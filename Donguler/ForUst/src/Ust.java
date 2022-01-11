718e589c793d9f79cbf9b12886aa176578c9c233
Bünyamin Tünç <bunyamintuncc@gmail.com>
2022-01-11 22:05:54 +0300

    for dongusu ile ust hesaplayan odev yapildi

---
+++ Donguler/ForUst/src/Ust.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ust {
+    public static void main(String[] args){
+        int sayi;
+        int ust;
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.print("Sayi : \t");
+        sayi=scanner.nextInt();
+        System.out.print("Ust : \t");
+        ust=scanner.nextInt();
+
+        int  sonuc=1;
+        for(int i=0;i<ust;i++){
+            sonuc*=sayi;
+        }
+        System.out.print("Sonuc : \t"+sonuc);
+    }
+}
